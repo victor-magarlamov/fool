c3cb85f21eef7ca9e8346f4f6b88d09eb6e22a9e
victor.magarlamov <victor.magarlamov@gmail.com>
2020-07-19 20:22:15 +0300

    Updates

---
+++ precache-manifest.089c5153061a29587d1a99bbad1d6c4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13d7c780d53372e1a91539d518bc469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c71b0528169a2a28eb53",
+    "url": "/static/css/main.5246fa10.chunk.css"
+  },
+  {
+    "revision": "7425300ad83c6990d8f6",
+    "url": "/static/js/2.c46129b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c46129b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71b0528169a2a28eb53",
+    "url": "/static/js/main.df49e83d.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
